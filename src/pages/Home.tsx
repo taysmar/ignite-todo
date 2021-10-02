b2cd945d13be3fcccc1a7d79ca0c633a4f630a56
Taysmar <taysmar@outlook.com>
2021-10-02 15:51:36 -0300

    Finalização do desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,18 +6,44 @@ import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
+
   const [tasks, setTasks] = useState<Task[]>([]);
 
+
+
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldTasks => [...oldTasks, newTask]);
+  };
+
+  //const task = tasks.find((task) => task.title === newTaskTitle);
+
+
+  //setTasks((oldState) => [...oldState, TasksList]);
+
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTask = tasks.map(task => ({ ...task }))
+
+    const foundItem = updatedTask.find(item => item.id === id);
+
+    if (!foundItem)
+      return;
+
+    foundItem.done = !foundItem.done;
+    setTasks(updatedTask);
+
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const updatedTask = tasks.filter(task => task.id !== id);
+    setTasks(updatedTask);
   }
 
   return (
@@ -26,10 +52,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
